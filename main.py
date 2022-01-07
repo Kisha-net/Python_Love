21f89415c3c014fc6d8bcbfce85ff53b2bbe65b4
Antoinette Akinyi <61618082+Kisha-net@users.noreply.github.com>
2022-01-07 23:55:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-class Person:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+class Car:
+    def __init__(self,speed=0):
+        self.speed = speed
+        self.odometer = 0
+        self.time = 0
 
-p1 = Person("Akinyi",20)
-p2 = Person("Antoinette",21)
+    def say_state(self):
+        print("I am going{}kph!".format(self.speed))
 
-print("My name is " + p1.name)
-print( p1.age)
-print("My name is " + p2.name)
+    def accelerate(self):
+        self.speed += 5
+
+    def brake(self):
+        if self.speed < 5:
+            self.speed = 0
+        else:
+            self.speed -= 5
+
+    def step(self):
+        self.odometer += self.speed
+        self.time += 1
+
+    def avarage_speed(self):
+        if self.time != 0:
+            return self.odometer / self.time
+        else:
+            pass
+
+if __name__ =='__main__':
+    my_car = Car()
+    print("I am a car!")
+    while True:
+        action = input("What should I do?[A]ccelerate, [B]rake, show[O]dometer, or show avarage[S]peed").upper()
+        if action not in "ABOS" or len(action) != 1:
+            print("I do not know that")
+            continue
+        if action == 'A':
+            my_car.accelerate()
+        elif action =='B':
+            my_car.brake()
+        elif action == 'O':
+            print("The car has been driven {} kilometers".format(my_car.odometer))
+        elif action == 'S':
+            print("The car has an avarage speed was {} kph".format(my_car.avarage_speed()))
+        my_car.step()
+        my_car.say_state()
 
 
