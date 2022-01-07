c3c9c3f4c44806f4e285fa50437b0d0aad73e38c
Antoinette Akinyi <61618082+Kisha-net@users.noreply.github.com>
2022-01-07 23:53:21 +0300

    my first python projects

---
+++ functions.py
@@ -0,0 +1,11 @@
+def myName():
+    global x
+    x = "Kisha"
+    print("Testing this global variable with yours truly "+ x)
+
+def myFun():
+    print ("my real name is " + x)
+
+myName()
+myFun()
+
---
+++ main.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Akinyi",20)
+p2 = Person("Antoinette",21)
+
+print("My name is " + p1.name)
+print( p1.age)
+print("My name is " + p2.name)
+
+
